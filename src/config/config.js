fe705536a456bbcb7d86bd3aaeaa412663a53f2f
Radon Rosborough <radon@intuitiveexplanations.com>
2024-02-26 20:42:11 -0800

    Update repo link

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 export const config = {
   retriesEnabled: true,
   message:
-    "Don't close this issue. This is an automatic message by [Fresh](https://github.com/c-hive/fresh) - a bot against stale bots.",
+    "Don't close this issue. This is an automatic message by [Fresh](https://github.com/raxod502/fresh) - a bot against stale bots.",
 };
 
 export function devEnv() {
